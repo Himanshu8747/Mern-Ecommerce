038318d43e95bc22b32f26121f2899a3b322c596
Himanshu <himanshu8747@gmail.com>
2024-01-31 15:18:38 +0530

    Add UserSchema, userModel , userController and userRoute

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -3,6 +3,8 @@ import express from 'express';
 const port = 4000;
 const app = express();
 
+app.use("/api/v1/user");
+
 app.listen(port,()=>{
     console.log(`Server is working on http://localhost:${port}`);
 })
---
+++ ecommerce-backend/src/routes/user.ts
@@ -0,0 +1,8 @@
+// Step 2 create routes
+
+import express from "express";
+
+const app = express.Router();
+
+//route will be user/new
+app.post("/new");
