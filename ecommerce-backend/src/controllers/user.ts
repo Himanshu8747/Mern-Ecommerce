a1c8ffbebed4d5abe4ddba3199407a48761dab17
Himanshu <himanshu8747@gmail.com>
2024-03-07 05:25:25 -0500

    Setting up Middleware

--- ecommerce-backend/src/controllers/user.ts
+++ ecommerce-backend/src/controllers/user.ts
@@ -1,17 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 import { User } from "../models/user.js";
+import { NewUserRequestBody } from "../types/types.js";
 
-export const newUser = async(req:Request,res:Response,next:NextFunction)=>{
+export const newUser = async(req:Request<{},{},NewUserRequestBody>,res:Response,next:NextFunction)=>{
     // creating new user
     try {
         // take incoming data from body
-        const {} = req.body
-        const user = await User.create({});
-        res.status(200).json({
+        const {name,email,photo,gender,_id,dob} = req.body
+        const user = await User.create({name,email,photo,gender,_id,dob: new Date(dob)});
+        return res.status(201).json({
             success:true,
             message:`Welcome, ${user.name}`,
         })
     } catch (error) {
-        
+        return res.status(400).json({
+            success:false,
+            message:error,
+        })
     }        
 }
