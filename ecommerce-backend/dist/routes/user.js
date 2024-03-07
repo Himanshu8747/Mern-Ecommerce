857ba53033a3319ba08f2161abb844aaf83e1f0b
Himanshu <himanshu8747@gmail.com>
2024-03-07 05:25:08 -0500

    Change files

---
+++ ecommerce-backend/dist/routes/user.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import { newUser } from '../controllers/user.js';
+const app = express.Router();
+// new User /api/v1/user/new
+app.post("/new", newUser);
+export default app;
