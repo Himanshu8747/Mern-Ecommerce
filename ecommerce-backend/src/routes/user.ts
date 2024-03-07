3aa6c4d55973da64b0998ccb079b5012cfa1bfde
Himanshu <himanshu8747@gmail.com>
2024-03-07 05:23:46 -0500

    Setup Database connection

--- ecommerce-backend/src/routes/user.ts
+++ ecommerce-backend/src/routes/user.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import { newUser } from '../controllers/user.js';
 
 const app = express.Router();
 
 // new User /api/v1/user/new
-app.post("/new");
+app.post("/new",newUser);
 
 export default app;
