0b44df661fa3eae2bf92d3d8ddf7d32f2789d235
Himanshu <himanshu8747@gmail.com>
2024-03-10 14:09:19 +0530

    Add route for getting all products

--- ecommerce-backend/dist/routes/product.js
+++ ecommerce-backend/dist/routes/product.js
@@ -1,15 +1,22 @@
 import express from 'express';
-import { deleteProduct, getAdminProducts, getAllCategories, getSingleProduct, getlatestProducts, newProduct, updateSingleProduct } from '../controllers/product.js';
+import { deleteProduct, getAdminProducts, getAllCategories, getAllProducts, getSingleProduct, getlatestProducts, newProduct, updateProduct } from '../controllers/product.js';
 import { singleUpload } from '../middlewares/multer.js';
+import { adminOnly } from '../middlewares/auth.js';
 const app = express.Router();
 // add new product
-app.post("/new", singleUpload, newProduct);
+app.post("/new", adminOnly, singleUpload, newProduct);
+// get all products with filter
+app.get("/all", getAllProducts);
 // get all latest products
 app.get("/latest", getlatestProducts);
 // get all categories
 app.get("/categories", getAllCategories);
 // get all admin products
-app.get("/admin-products", getAdminProducts);
+app.get("/admin-products", adminOnly, getAdminProducts);
 // updating and deleting
-app.route("/:id").get(getSingleProduct).put(updateSingleProduct).delete(deleteProduct);
+app
+    .route("/:id")
+    .get(getSingleProduct)
+    .put(adminOnly, singleUpload, updateProduct)
+    .delete(adminOnly, deleteProduct);
 export default app;
