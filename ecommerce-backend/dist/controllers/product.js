39f06fb127b2b53d4247465a5dee0a4995c6b503
Himanshu <himanshu8747@gmail.com>
2024-03-10 14:09:45 +0530

    Add controller for getting products by filters

--- ecommerce-backend/dist/controllers/product.js
+++ ecommerce-backend/dist/controllers/product.js
@@ -2,8 +2,10 @@ import { TryCatch } from "../middlewares/error.js";
 import { Product } from "../models/product.js";
 import ErrorHandler from "../utils/utility-class.js";
 import { rm } from "fs";
+// import {faker} from "@faker-js/faker";
 export const newProduct = TryCatch(async (req, res, next) => {
     const { name, price, stock, category } = req.body;
+    console.log(name, price, stock, category);
     const photo = req.file;
     if (!photo)
         return next(new ErrorHandler("Please add photo", 401));
@@ -51,15 +53,14 @@ export const getSingleProduct = TryCatch(async (req, res, next) => {
         product
     });
 });
-export const updateSingleProduct = TryCatch(async (req, res, next) => {
-    const id = req.params.id;
-    if (!id)
-        return next(new ErrorHandler("Invalid Product ID", 404));
+export const updateProduct = TryCatch(async (req, res, next) => {
+    const { id } = req.params;
     const { name, price, stock, category } = req.body;
+    console.log(name, price, stock, category);
     const photo = req.file;
     const product = await Product.findById(id);
     if (!product)
-        return next(new ErrorHandler("Product not found", 404));
+        return next(new ErrorHandler("Product Not Found", 404));
     if (photo) {
         rm(product.photo, () => {
             console.log("Old Photo Deleted");
@@ -75,9 +76,11 @@ export const updateSingleProduct = TryCatch(async (req, res, next) => {
     if (category)
         product.category = category;
     await product.save();
+    console.log(product.name);
     return res.status(200).json({
         success: true,
-        message: "Product updated sucessfully"
+        product,
+        message: "Product Updated Successfully",
     });
 });
 export const deleteProduct = TryCatch(async (req, res, next) => {
@@ -95,3 +98,65 @@ export const deleteProduct = TryCatch(async (req, res, next) => {
         message: "Product deleted sucessfully"
     });
 });
+// filter functionality of products all
+export const getAllProducts = TryCatch(async (req, res, next) => {
+    const { search, sort, category, price } = req.query;
+    const page = Number(req.query.page) || 1;
+    const limit = Number(process.env.PRODUCT_PER_PAGE) || 8;
+    const skip = (page - 1) * limit;
+    const baseQuery = {};
+    if (search) {
+        baseQuery.name = {
+            $regex: search,
+            $options: "i",
+        };
+    }
+    if (price) {
+        baseQuery.price = {
+            $lte: Number(price),
+        };
+    }
+    if (category) {
+        baseQuery.category = category;
+    }
+    const [products, filteredOnlyProduct] = await Promise.all([
+        Product.find(baseQuery)
+            .sort(sort ? { price: sort === "asc" ? 1 : -1 } : undefined)
+            .limit(limit).skip(skip),
+        Product.find(baseQuery)
+    ]);
+    const totalPage = Math.ceil(filteredOnlyProduct.length / limit);
+    return res.status(200).json({
+        success: true,
+        products,
+        totalPage,
+    });
+});
+// const deleteRandomProducts = async(count:number=10)=>{
+//     const products = await Product.find({}).skip(2);
+//     for(let i=0;i<products.length;i++){
+//         const product = products[i];
+//         await product.deleteOne();
+//     }
+//     console.log({success:true});
+// }
+// deleteRandomProducts(38);
+// const generateRandomProducts = async(count:number=10)=>{
+//     const products=[];
+//     for(let i=0;i<count;i++){
+//         const product = {
+//             name:faker.commerce.productName(),
+//             photo:"uploads\\5c08d563-2f39-480f-a0aa-3e62ff8f241e.jpg",
+//             price:faker.commerce.price({min:1500,max:100000,dec:0}),
+//             stock:faker.commerce.price({min:0,max:100,dec:0}),
+//             category:faker.commerce.department(),
+//             createdAt:new Date(faker.date.past()),
+//             updatedAt:new Date(faker.date.recent()),
+//             _v:0
+//         };
+//         products.push(product);
+//     }
+//     await Product.create(products);
+//     console.log({success:true});
+// }
+// generateRandomProducts(40);
--- ecommerce-backend/src/controllers/product.ts
+++ ecommerce-backend/src/controllers/product.ts
@@ -1,13 +1,15 @@
 import { Request } from "express";
 import { TryCatch } from "../middlewares/error.js";
-import { NewProductRequestBody } from "../types/types.js";
+import { NewProductRequestBody, SearchRequestQuery, baseQueryType } from "../types/types.js";
 import { Product } from "../models/product.js";
 import ErrorHandler from "../utils/utility-class.js";
 import { rm } from "fs";
+// import {faker} from "@faker-js/faker";
 
 export const newProduct = TryCatch(
     async(req:Request<{},{},NewProductRequestBody>,res,next)=>{
     const {name,price,stock,category} =req.body;
+    console.log(name,price,stock,category);
     const photo = req.file;
     if(!photo) return next(new ErrorHandler("Please add photo",401));
     if(!name||!price||!stock||!category) {
@@ -55,35 +57,36 @@ export const getSingleProduct = TryCatch(async(req,res,next)=>{
     })
 });
 
-export const updateSingleProduct = TryCatch(
-    async(req,res,next)=>{
-    const id = req.params.id    
-    if(!id) return next(new ErrorHandler("Invalid Product ID",404));
-    const {name,price,stock,category} =req.body;
+export const updateProduct = TryCatch(async (req, res, next) => {
+    const { id } = req.params;
+    const { name, price, stock, category } = req.body;
+    console.log(name,price,stock,category);
     const photo = req.file;
     const product = await Product.findById(id);
-
-    if(!product) return next(new ErrorHandler("Product not found",404));
-
-    if(photo){
-        rm(product.photo!,()=>{
-            console.log("Old Photo Deleted");
-        })
-
-        product.photo = photo.path;
+  
+    if (!product) return next(new ErrorHandler("Product Not Found", 404));
+  
+    if (photo) {
+      rm(product.photo!, () => {
+        console.log("Old Photo Deleted");
+      });
+      product.photo = photo.path;
     }
-    if(name)product.name = name;
-    if(price)product.price = price;
-    if(stock)product.stock = stock;
-    if(category)product.category = category;
-
+  
+    if (name) product.name = name;
+    if (price) product.price = price;
+    if (stock) product.stock = stock;
+    if (category) product.category = category;
+  
     await product.save();
+    console.log(product.name);
     return res.status(200).json({
-        success:true,
-        message:"Product updated sucessfully"
-    })
-
-});
+      success: true,
+      product,
+      message: "Product Updated Successfully",
+    });
+  });
+  
 
 export const deleteProduct = TryCatch(async(req,res,next)=>{
     const product = await Product.findById(req.params.id);
@@ -99,3 +102,75 @@ export const deleteProduct = TryCatch(async(req,res,next)=>{
         message:"Product deleted sucessfully"
     })
 })
+
+
+// filter functionality of products all
+
+export const getAllProducts = TryCatch(async(req:Request<{},{},{},SearchRequestQuery>,res,next)=>{
+    const {search,sort,category,price} = req.query;
+    const page = Number(req.query.page) ||1;
+    const limit = Number(process.env.PRODUCT_PER_PAGE) || 8;
+    const skip = (page-1)*limit;
+    const baseQuery:baseQueryType = {};
+    if(search){
+        baseQuery.name = {
+            $regex:search,
+            $options:"i",
+        }
+    }
+    if(price){
+        baseQuery.price = {
+            $lte:Number(price),
+        }
+    }
+    if(category){
+        baseQuery.category = category;
+    }
+
+    const [products,filteredOnlyProduct] = await Promise.all([
+        Product.find(baseQuery)
+        .sort(sort?{price:sort==="asc"?1:-1}:undefined)
+        .limit(limit).skip(skip),
+        Product.find(baseQuery)
+    ])
+    const totalPage = Math.ceil(filteredOnlyProduct.length/limit);
+
+    return res.status(200).json({
+        success:true,
+        products,
+        totalPage,
+    })
+});
+
+
+
+// const deleteRandomProducts = async(count:number=10)=>{
+//     const products = await Product.find({}).skip(2);
+//     for(let i=0;i<products.length;i++){
+//         const product = products[i];
+//         await product.deleteOne();
+//     }
+//     console.log({success:true});
+// }
+
+// deleteRandomProducts(38);
+// const generateRandomProducts = async(count:number=10)=>{
+//     const products=[];
+//     for(let i=0;i<count;i++){
+//         const product = {
+//             name:faker.commerce.productName(),
+//             photo:"uploads\\5c08d563-2f39-480f-a0aa-3e62ff8f241e.jpg",
+//             price:faker.commerce.price({min:1500,max:100000,dec:0}),
+//             stock:faker.commerce.price({min:0,max:100,dec:0}),
+//             category:faker.commerce.department(),
+//             createdAt:new Date(faker.date.past()),
+//             updatedAt:new Date(faker.date.recent()),
+//             _v:0
+//         };
+//         products.push(product);
+//     }
+//     await Product.create(products);
+//     console.log({success:true});
+// }
+
+// generateRandomProducts(40);
