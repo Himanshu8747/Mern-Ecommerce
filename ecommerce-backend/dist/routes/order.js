267b23981793037c5e808340ed172256a2f7b372
Himanshu <himanshu8747@gmail.com>
2024-03-15 10:13:46 +0530

    Add routes for deleting the product and Processing the product

--- ecommerce-backend/dist/routes/order.js
+++ ecommerce-backend/dist/routes/order.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { adminOnly } from '../middlewares/auth.js';
-import { myOrders, newOrder, allOrders, getSingleOrder } from '../controllers/order.js';
+import { myOrders, newOrder, allOrders, getSingleOrder, processOrder, deleteOrder } from '../controllers/order.js';
 const app = express.Router();
 // new User /api/v1/user/new
 app.post("/new", newOrder);
 app.get("/my", myOrders);
 app.get("/all", adminOnly, allOrders);
-app.route("/:id").get(getSingleOrder);
+app.route("/:id").get(getSingleOrder).put(adminOnly, processOrder).delete(adminOnly, deleteOrder);
 export default app;
--- ecommerce-backend/src/routes/order.ts
+++ ecommerce-backend/src/routes/order.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { adminOnly } from '../middlewares/auth.js';
-import { myOrders, newOrder, allOrders, getSingleOrder } from '../controllers/order.js';
+import { myOrders, newOrder, allOrders, getSingleOrder, processOrder, deleteOrder } from '../controllers/order.js';
 
 const app = express.Router();
 
@@ -12,7 +12,7 @@ app.get("/my",myOrders);
 
 app.get("/all",adminOnly,allOrders);
 
-app.route("/:id").get(getSingleOrder);
+app.route("/:id").get(getSingleOrder).put(adminOnly,processOrder).delete(adminOnly,deleteOrder);
 
 
 
