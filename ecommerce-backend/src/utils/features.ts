bae027175a7fb9cbe54ef9e15444278aeb121f87
Himanshu <himanshu8747@gmail.com>
2024-04-10 10:09:32 +0530

    Update

--- ecommerce-backend/src/utils/features.ts
+++ ecommerce-backend/src/utils/features.ts
@@ -1,8 +1,7 @@
-import mongoose from 'mongoose';
-import { OrderItemType, InvalidateCacheProps } from '../types/types.js';
-import { Product } from '../models/product.js';
-import { myCache } from '../app.js';
-import { Order } from '../models/order.js';
+import mongoose, { Document } from "mongoose";
+import { myCache } from "../app.js";
+import { Product } from "../models/product.js";
+import { InvalidateCacheProps, OrderItemType } from "../types/types.js";
 
 export const connectDB=()=>{
     mongoose.connect("mongodb://localhost:27017",{
@@ -11,41 +10,119 @@ export const connectDB=()=>{
     .catch((e)=>console.log(e));
 }
 
-export const invalidateCache=async({product,order,admin,productId}:InvalidateCacheProps)=>{
-    if(product){
-        const productKeys:string[]=["latest-products","categories","all-products", `product-${productId}`];
-        
-        // for getSingle Product usig ID;
-        const products = await Product.find({}).select("_id");
-        products.forEach(element => {
-            productKeys.push(`product-${element._id}`);
-        });
+export const invalidateCache = ({
+  product,
+  order,
+  admin,
+  userId,
+  orderId,
+  productId,
+}: InvalidateCacheProps) => {
+  if (product) {
+    const productKeys: string[] = [
+      "latest-products",
+      "categories",
+      "all-products",
+    ];
 
-        myCache.del(productKeys);
-    }
-    if(order){
-        const orderKeys:string[] = ['all-orders'];
-        const orders = await Order.find({}).select("_id");
-        orders.forEach(i => {
-            orderKeys.push(`order-${i._id}`);
-        });
+    if (typeof productId === "string") productKeys.push(`product-${productId}`);
 
-        myCache.del(orderKeys);
-    }
-    if(admin){
+    if (typeof productId === "object")
+      productId.forEach((i) => productKeys.push(`product-${i}`));
 
-    }
+    myCache.del(productKeys);
+  }
+  if (order) {
+    const ordersKeys: string[] = [
+      "all-orders",
+      `my-orders-${userId}`,
+      `order-${orderId}`,
+    ];
+
+    myCache.del(ordersKeys);
+  }
+  if (admin) {
+    myCache.del([
+      "admin-stats",
+      "admin-pie-charts",
+      "admin-bar-charts",
+      "admin-line-charts",
+    ]);
+  }
+};
+
+export const reduceStock = async (orderItems: OrderItemType[]) => {
+  for (let i = 0; i < orderItems.length; i++) {
+    const order = orderItems[i];
+    const product = await Product.findById(order.productId);
+    if (!product) throw new Error("Product Not Found");
+    product.stock -= order.quantity;
+    await product.save();
+  }
+};
+
+export const calculatePercentage = (thisMonth: number, lastMonth: number) => {
+  if (lastMonth === 0) return thisMonth * 100;
+  const percent = (thisMonth / lastMonth) * 100;
+  return Number(percent.toFixed(0));
+};
+
+export const getInventories = async ({
+  categories,
+  productsCount,
+}: {
+  categories: string[];
+  productsCount: number;
+}) => {
+  const categoriesCountPromise = categories.map((category) =>
+    Product.countDocuments({ category })
+  );
+
+  const categoriesCount = await Promise.all(categoriesCountPromise);
+
+  const categoryCount: Record<string, number>[] = [];
+
+  categories.forEach((category, i) => {
+    categoryCount.push({
+      [category]: Math.round((categoriesCount[i] / productsCount) * 100),
+    });
+  });
+
+  return categoryCount;
+};
+
+interface MyDocument extends Document {
+  createdAt: Date;
+  discount?: number;
+  total?: number;
 }
+type FuncProps = {
+  length: number;
+  docArr: MyDocument[];
+  today: Date;
+  property?: "discount" | "total";
+};
 
-export const reduceStock=async(orderItems:OrderItemType[]) =>{
-    for (let i = 0; i< orderItems.length; i++) {
-       const order = orderItems[i];
-       const product = await Product.findById(order.productId);
-       if(!product){
-        throw new Error("Product not found !");
-       } 
-       product.stock-=order.quantity;
-       await product.save();
+export const getChartData = ({
+  length,
+  docArr,
+  today,
+  property,
+}: FuncProps) => {
+  const data: number[] = new Array(length).fill(0);
+
+  docArr.forEach((i) => {
+    const creationDate = i.createdAt;
+    const monthDiff = (today.getMonth() - creationDate.getMonth() + 12) % 12;
+
+    if (monthDiff < length) {
+      if (property) {
+        data[length - monthDiff - 1] += i[property]!;
+      } else {
+        data[length - monthDiff - 1] += 1;
+      }
     }
+  });
 
-}
+  return data;
+};
