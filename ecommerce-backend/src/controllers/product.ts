087311208d1fe2abbf0854fef5cef06ccb79acd6
Himanshu <himanshu8747@gmail.com>
2024-04-10 10:08:25 +0530

    Add pfroduct Id in Product Controller for updateProduct and deleteProduct

--- ecommerce-backend/src/controllers/product.ts
+++ ecommerce-backend/src/controllers/product.ts
@@ -123,7 +123,7 @@ export const updateProduct = TryCatch(async (req, res, next) => {
   
     await product.save();
 
-    await invalidateCache({product:true});
+    await invalidateCache({product:true, productId: String(product._id)});
 
     return res.status(200).json({
       success: true,
@@ -143,7 +143,7 @@ export const deleteProduct = TryCatch(async(req,res,next)=>{
     }
     await product.deleteOne();
 
-    await invalidateCache({product:true});
+    await invalidateCache({product:true,productId: String(product._id)});
     
     return res.status(200).json({
         success:true,
