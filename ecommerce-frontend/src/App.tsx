81da134cdac8433998905a5917d4aca3a65525c9
Himanshu <himanshu8747@gmail.com>
2024-04-16 13:15:23 +0530

    Add Protected Routes

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -1,21 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { lazy,Suspense, useEffect } from "react";
-const  Home = lazy(()=>import( "./pages/Home"));
-const  Search =  lazy(()=>import("./pages/Search"));
-const  Cart =  lazy(()=>import("./pages/Cart"));
+import { lazy, Suspense, useEffect } from "react";
+const Home = lazy(() => import("./pages/Home"));
+const Search = lazy(() => import("./pages/Search"));
+const Cart = lazy(() => import("./pages/Cart"));
 import Loader from "./component/Loader";
 import Header from "./component/Header";
 import Login from "./pages/Login";
 import Orders from "./pages/Orders";
 import OrderDetails from "./pages/OrderDetails";
-const  Shipping =lazy(()=>import("./pages/Shipping"));  
-import {Toaster} from 'react-hot-toast'
+const Shipping = lazy(() => import("./pages/Shipping"));
+import { Toaster } from "react-hot-toast";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
-import { userExist,userNotExist} from "./redux/reducer/userReducer";
-import { useDispatch,useSelector } from "react-redux";
+import { userExist, userNotExist } from "./redux/reducer/userReducer";
+import { useDispatch, useSelector } from "react-redux";
 import { UserReducerInitialState } from "./types/reducer-types";
 import { getUser } from "./redux/api/userAPI";
+import ProtectedRoute from "./component/ProtectedRoute";
 // Admin Routes Importing
 
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
@@ -36,10 +37,9 @@ const TransactionManagement = lazy(
   () => import("./pages/admin/management/transactionmanagement")
 );
 
-
 const App = () => {
   const { user, loading } = useSelector(
-    (state:{userReducer:UserReducerInitialState}) => state.userReducer
+    (state: { userReducer: UserReducerInitialState }) => state.userReducer
   );
 
   const dispatch = useDispatch();
@@ -53,60 +53,74 @@ const App = () => {
     });
   }, []);
 
-    return loading ? (
-      <Loader />
-    ) : (
+  return loading ? (
+    <Loader />
+  ) : (
     <Router>
-      <Header user={user}/>
-      <Suspense fallback={<Loader/>}>
+      <Header user={user} />
+      <Suspense fallback={<Loader />}>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/search" element={<Search/>}/>
-          <Route path='/cart' element={<Cart/>}/>
-          {/* Not Logged in */}
-          <Route path='/login' element={<Login/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+          {/* Not logged In Route */}
+          <Route
+            path="/login"
+            element={
+              <ProtectedRoute isAuthenticated={user ? false : true}>
+                <Login />
+              </ProtectedRoute>
+            }
+          />
           {/* Logged In User Routes */}
-          <Route>
-          <Route path='/shipping' element={<Shipping/>}/>
-          <Route path="/orders" element={<Orders/>}/>
-          <Route path="/order/:id" element={<OrderDetails/>}/>
+          <Route
+            element={<ProtectedRoute isAuthenticated={user ? true : false} />}
+          >
+            <Route path="/shipping" element={<Shipping />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/order/:id" element={<OrderDetails />} />
           </Route>
 
-
-          
           {/* Admin Routes */}
-          {/* <Route
-  element={
-    <ProtectedRoute isAuthenticated={true} adminRoute={true} isAdmin={true} />
-  } */}
-{/* > */}
-  <Route path="/admin/dashboard" element={<Dashboard />} />
-  <Route path="/admin/product" element={<Products />} />
-  <Route path="/admin/customer" element={<Customers />} />
-  <Route path="/admin/transaction" element={<Transaction />} />
-  {/* Charts */}
-  <Route path="/admin/chart/bar" element={<Barcharts />} />
-  <Route path="/admin/chart/pie" element={<Piecharts />} />
-  <Route path="/admin/chart/line" element={<Linecharts />} />
-  {/* Apps */}
-  <Route path="/admin/app/coupon" element={<Coupon />} />
-  <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
-  <Route path="/admin/app/toss" element={<Toss />} />
+          <Route
+            element={
+              <ProtectedRoute
+                isAuthenticated={true}
+                adminOnly={true}
+                admin={user?.role === "admin" ? true : false}
+              />
+            }
+          />
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/product" element={<Products />} />
+          <Route path="/admin/customer" element={<Customers />} />
+          <Route path="/admin/transaction" element={<Transaction />} />
+          {/* Charts */}
+          <Route path="/admin/chart/bar" element={<Barcharts />} />
+          <Route path="/admin/chart/pie" element={<Piecharts />} />
+          <Route path="/admin/chart/line" element={<Linecharts />} />
+          {/* Apps */}
+          <Route path="/admin/app/coupon" element={<Coupon />} />
+          <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+          <Route path="/admin/app/toss" element={<Toss />} />
 
-  {/* Management */}
-  <Route path="/admin/product/new" element={<NewProduct />} />
+          {/* Management */}
+          <Route path="/admin/product/new" element={<NewProduct />} />
 
-  <Route path="/admin/product/:id" element={<ProductManagement />} />
+          <Route path="/admin/product/:id" element={<ProductManagement />} />
 
-  <Route path="/admin/transaction/:id" element={<TransactionManagement />} />
-{/* </Route>; */}
+          <Route
+            path="/admin/transaction/:id"
+            element={<TransactionManagement />}
+          />
+          {/* </Route>; */}
         </Routes>
       </Suspense>
-      <Toaster position="bottom-center"/>
-    </Router> 
-  )
-}
+      <Toaster position="bottom-center" />
+    </Router>
+  );
+};
 
-// Firebase wala chal nahi raha hai wo karna hai ! last me 
+// Firebase wala chal nahi raha hai wo karna hai ! last me
 
-export default App
+export default App;
