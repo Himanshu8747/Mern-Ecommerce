ff6623ab1c51ee76ef58609ca955e7ca83d9b5cd
Himanshu <himanshu8747@gmail.com>
2024-04-18 13:40:02 +0530

    Add skeleton Loader

--- ecommerce-frontend/src/component/Loader.tsx
+++ ecommerce-frontend/src/component/Loader.tsx
@@ -5,3 +5,14 @@ const Loader = () => {
 }
 
 export default Loader
+
+export const Skeleton = ({width="unset"}:{width:string}) => {
+  return (
+    <div className="skeleton-loader" style={{width}}>
+    <div className="skeleton-shape"></div>
+    <div className="skeleton-shape"></div>
+    <div className="skeleton-shape"></div>
+  </div>
+  )
+  
+}
