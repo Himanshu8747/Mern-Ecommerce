cc1ac2c513b7741d333677618d75cb045cb0157c
Himanshu <himanshu8747@gmail.com>
2024-04-18 13:41:12 +0530

    Get Products and add logic for addToCartHandler

--- ecommerce-frontend/src/pages/Home.tsx
+++ ecommerce-frontend/src/pages/Home.tsx
@@ -1,17 +1,36 @@
-import { Link } from "react-router-dom"
-import ProductCard from "../component/ProductCard"
+import { Link } from "react-router-dom";
+import ProductCard from "../component/ProductCard";
+import { useLatestProductsQuery } from "../redux/api/productApi";
+import toast from "react-hot-toast";
+import {Skeleton } from "../component/Loader";
 
 const Home = () => {
-  const addToCartHandler=()=>{}
-  return (
+  const { data,isLoading,isError} = useLatestProductsQuery("");
+  const addToCartHandler = () => {};
+  if(isError){
+    toast.error("Cannot fetch the products");
+  }
+  return (  
     <div className="home">
       <section></section>
       <h1>Latest Products <Link className="findmore" to={"/search"}>More</Link></h1>
       <main>
-        <ProductCard productId="sdfgs" name="MacVook" price={876876} stock={21121} handler={addToCartHandler} photo="https://m.media-amazon.com/images/I/71TPda7cwUL._SX522_.jpg" />
+        {isLoading ? (<Skeleton width="80vw"/> ) : (
+          data?.products.map((product) => (
+          <ProductCard
+            key={product._id}
+            productId={product._id}
+            name={product.name}
+            price={product.price}
+            stock={product.stock}
+            handler={addToCartHandler}
+            photo={product.photo}
+          />
+        ))
+      )}
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
