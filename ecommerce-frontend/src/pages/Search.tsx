e3c2b8bb5ca88d84c308a9bb411b61c10fda0449
Himanshu <himanshu8747@gmail.com>
2024-04-19 10:26:43 +0530

    Add serachFunctionality

--- ecommerce-frontend/src/pages/Search.tsx
+++ ecommerce-frontend/src/pages/Search.tsx
@@ -1,16 +1,34 @@
 import { useState } from "react"
 import ProductCard from "../component/ProductCard";
+import { useCategoriesQuery, useSearchProductsQuery } from "../redux/api/productApi";
+import { CustomError } from "../types/api-types";
+import toast from "react-hot-toast";
+import { Skeleton } from "../component/Loader";
 
 const Search = () => {
+
+  const {data:categoriesResponse,isLoading:loadingCategories,isError:searchedProductHasError,error:searchedProductError} = useCategoriesQuery("");
+  
   const[search,setSearch] = useState("");
   const[sort,setSort] = useState("");
   const[maxPrice,setMaxPrice]=useState(100000);
   const[category,setCategory] = useState("");
   const[page,setPage] = useState(1);
 
+  const {isLoading:productLoading,data:searchedData,isError,error} = useSearchProductsQuery({search,sort,category,page,price:maxPrice})
+
   const addToCartHandler=()=>{}
   const isNextPage = page < 4 ;
   const isPrevPage = page > 1;
+
+  if(isError){
+    const err = error as CustomError;
+    toast.error(err.data.message);
+  }
+  if(searchedProductHasError){
+    const err = searchedProductError as CustomError;
+    toast.error(err.data.message);
+  }
 
   return (
     <div className="product-search-page">
@@ -32,8 +50,9 @@ const Search = () => {
           <h4>Category</h4>
           <select value={category} onChange={(e)=>setCategory(e.target.value)}>
             <option value="">All</option>
-            <option value="">Sample 1</option>
-            <option value="">Sample 2</option>
+            {!loadingCategories && categoriesResponse?.categories.map((i)=>(
+              <option key={i} value={i}>{i.toUpperCase()}</option>
+            ))}
           </select>
         </div>
         <div></div>
@@ -41,14 +60,28 @@ const Search = () => {
       <main>
         <h1>Products</h1>
         <input type="text" placeholder="Search by name..." value={search} onChange={(e)=>setSearch(e.target.value)}/>
-        <div className="search-product-list">
-        <ProductCard productId="sdfgs" name="MacVook" price={876876} stock={21121} handler={addToCartHandler} photo="https://m.media-amazon.com/images/I/71TPda7cwUL._SX522_.jpg" />
+        {productLoading ? <Skeleton length={10}/> : (
+          <div className="search-product-list">
+          {searchedData?.products.map(i=>(
+            <ProductCard
+              key={i._id}
+              productId={i._id}
+              name={i.name}
+              price={i.price}
+              stock={i.stock}
+              photo={i.photo}
+              handler={addToCartHandler}
+            />
+          ))}
         </div>
-        <article>
-          <button disabled={!isPrevPage} onClick={()=>setPage(prev=>prev-1)}>Prev</button>
-          <span>{page} of {5}</span>
-          <button disabled={!isNextPage} onClick={()=>setPage(prev=>prev+1)}>Next</button>
-        </article>
+        )}
+          {searchedData && searchedData.totalPage > 1 && (
+             <article>
+             <button disabled={!isPrevPage} onClick={()=>setPage(prev=>prev-1)}>Prev</button>
+             <span>{page} of {searchedData.totalPage}</span>
+             <button disabled={!isNextPage} onClick={()=>setPage(prev=>prev+1)}>Next</button>
+           </article>
+          )} 
       </main>
     </div>
   )
