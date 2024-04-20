98d6d2f08307c285399a7c6324530035dbdcac5a
Himanshu <himanshu8747@gmail.com>
2024-04-20 10:55:33 +0530

    Add types for ProductDetail, DeleteProduct and UpdateProduct

--- ecommerce-frontend/src/types/api-types.ts
+++ ecommerce-frontend/src/types/api-types.ts
@@ -1,37 +1,56 @@
-import { Product, User } from "./types"
+import { Product, User } from "./types";
 
 export type MessageResponse = {
-    success:boolean,
-    message:string
-}
+  success: boolean;
+  message: string;
+};
 export type UserResponse = {
-    success:boolean,
-    user:User
-}
+  success: boolean;
+  user: User;
+};
 
 export type AllProductsResponse = {
-    success:boolean,
-    products:Product[],
-}
+  success: boolean;
+  products: Product[];
+};
 export type CategoriesResponse = {
-    success:boolean,
-    categories:string[],
-}
+  success: boolean;
+  categories: string[];
+};
 export type searchProductsResponse = AllProductsResponse & {
-    totalPage:number
-}
+  totalPage: number;
+};
 export type searchProductsRequest = {
-    price:number,
-    page:number,
-    category:string,
-    search:string,
-    sort:string,
-}
+  price: number;
+  page: number;
+  category: string;
+  search: string;
+  sort: string;
+};
+export type NewProductRequest = {
+  id: string;
+  formData: FormData;
+};
+
+export type ProductResponse = {
+  success: boolean;
+  product: Product;
+};
+
+export type DeleteProductRequest = {
+  userId: string;
+  productId: string;
+};
+export type UpdateProductRequest = {
+  userId: string;
+  productId: string;
+  formData: FormData;
+};
 
 export type CustomError = {
-    status:number,
-    data:{
-        message:string,
-        success:boolean
-    }
-}
+  status: number;
+  data: {
+    message: string;
+    success: boolean;
+  };
+};
