fc03ee82ff3a2bcf7fa7dd9d85e0995f45d6b472
Himanshu <himanshu8747@gmail.com>
2024-04-23 10:57:09 +0530

    Add cartItme in Product card handler

--- ecommerce-frontend/src/component/ProductCard.tsx
+++ ecommerce-frontend/src/component/ProductCard.tsx
@@ -1,27 +1,41 @@
 import { FaPlus } from "react-icons/fa";
 import { server } from "../redux/store";
+import { CartItem } from "../types/types";
 
 type ProductsProps = {
-    productId:string;
-    photo:string;
-    name:string;
-    price:number;
-    stock:number;
-    handler:()=>void;
-}
+  productId: string;
+  photo: string;
+  name: string;
+  price: number;
+  stock: number;
+  handler: (cartItem: CartItem) => string | undefined;
+};
 
-
-const ProductCard = ({productId,price,photo,name,stock,handler}:ProductsProps) => {
+const ProductCard = ({
+  productId,
+  price,
+  name,
+  photo,
+  stock,
+  handler,
+}: ProductsProps) => {
   return (
-    <div className="productcard">
-        <img src={`${server}/${photo}`} alt={name}></img>
-        <p>{name}</p>
-        <span>{price}</span>
-        <div>
-            <button onClick={()=>handler()}><FaPlus/></button>
-        </div>
-    </div>
-  )
-}
+    <div className="product-card">
+      <img src={`${server}/${photo}`} alt={name} />
+      <p>{name}</p>
+      <span>₹{price}</span>
 
-export default ProductCard
+      <div>
+        <button
+          onClick={() =>
+            handler({ productId, price, name, photo, stock, quantity: 1 })
+          }
+        >
+          <FaPlus />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
